ea412bf538c28579609c7e95090e11830e64b828
Jungir <googlman6300@gmail.com>
2019-08-14 14:11:54 +0600

    connected to db

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/graphql', {useNewUrlParser: true})
+    .then(()=>{console.log('connected to db')})
+    .catch((err)=> {console.log('there is and error:', err)});
 
 app.use('/graphql', graphqlHTTP({
     schema,
