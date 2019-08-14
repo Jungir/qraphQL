5d8fd7e66f11bd780a412e0ee7227805dd0afc49
Jungir <googlman6300@gmail.com>
2019-08-14 12:06:40 +0600

    Root Query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,11 @@
 const graphql = require('graphql');
-// graphql.GraphQLString
-const {GraphQLObjectType, GraphQLString} = graphql;
+// graphql.
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
+//schema file has 3 responsibilites:
+//1. To define types: Booktype
+//2. define relationships b-n types: Books <-> Authors
+//3. Queries: root query, Book, Author root queries
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields : () => ({
@@ -9,4 +13,21 @@ const BookType = new GraphQLObjectType({
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     }),
+});
+
+const RootQuery = new GraphQLObjectType({
+    name : 'RootQueryType',
+    fields : {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                // code to get data from db/other source
+                // args.id
+            }
+        }
+    }
+})
+module.exports = new GraphQLSchema({
+    query : RootQuery
 });
